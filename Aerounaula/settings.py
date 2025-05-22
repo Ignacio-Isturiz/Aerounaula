51687a9c6b8a844f7540b4a1eb1c01d02e94a07b
unknown <ignacioisturizr@gmail.com>
2025-05-22 18:36:41 -0500

    fix: databases in seetings and options for neon.tech

--- Aerounaula/settings.py
+++ Aerounaula/settings.py
@@ -112,6 +112,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',  # Necesario para Neon.tech
+        },
     }
 }
 
