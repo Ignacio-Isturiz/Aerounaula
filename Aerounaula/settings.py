820beb4e690ce36f906e6eb25c72a106ead3ae77
juliloa <juliana.loaiza14@gmail.com>
2025-05-22 12:05:02 -0500

    fix: databases, name

--- Aerounaula/settings.py
+++ Aerounaula/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'Aerounaula.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': tmpPostgres.path.decode().replace('/', ''),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
