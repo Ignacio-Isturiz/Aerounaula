ab4c74713f82fff28ab16c5b8f9385a4e5ffb2c1
unknown <ignacioisturizr@gmail.com>
2025-05-25 11:55:55 -0500

    fix: deploy with emails, attempt 2

--- Aerounaula/settings.py
+++ Aerounaula/settings.py
@@ -178,8 +178,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ.get['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 AXES_LOCK_OUT_AT_FAILURE = True 
